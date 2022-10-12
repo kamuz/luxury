b5a07130299bddbc2de59ca53f94ac52cf8960c4
Vladimir Kamuz <v.kamuz@gmail.com>
2022-10-12 05:36:24 +0300

    Code format

--- index.php
+++ index.php
@@ -2,15 +2,9 @@
 	<div class="bnr" id="home">
 		<div  id="top" class="callbacks_container">
 			<ul class="rslides" id="slider4">
-				<li>
-					<img src="assets/images/bnr-1.jpg" alt=""/>
-				</li>
-				<li>
-					<img src="assets/images/bnr-2.jpg" alt=""/>
-				</li>
-				<li>
-					<img src="assets/images/bnr-3.jpg" alt=""/>
-				</li>
+				<li><img src="assets/images/bnr-1.jpg" alt=""/></li>
+				<li><img src="assets/images/bnr-2.jpg" alt=""/></li>
+				<li><img src="assets/images/bnr-3.jpg" alt=""/></li>
 			</ul>
 		</div>
 		<div class="clearfix"> </div>
